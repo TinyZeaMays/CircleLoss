cbebe28c33fa35fc5a0eef90953b42e239bf2a05
tzm <1049793129@qq.com>
2020-03-31 00:44:11 +0800

    init

---
+++ circle_loss.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn, Tensor
+
+
+class NormLinear(nn.Linear):
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super(NormLinear, self).__init__(in_features, out_features, bias=False)
+
+    def forward(self, inp: Tensor) -> Tensor:
+        return nn.functional.linear(inp, nn.functional.normalize(self.weight))
+
+
+# TODO: check detach
+class CircleLoss(nn.Module):
+    def __init__(self, m: float, gamma: float) -> None:
+        super(CircleLoss, self).__init__()
+        self.m = m
+        self.gamma = gamma
+        self.loss = nn.CrossEntropyLoss()
+
+    def forward(self, inp: Tensor, label: Tensor) -> Tensor:
+        a = torch.clamp_min(inp + self.m, min=0).detach()
+        a[label] = torch.clamp_min(- inp[label] + 1 + self.m, min=0).detach()
+        sigma = torch.ones_like(inp, device=inp.device, dtype=inp.dtype) * self.m
+        sigma[label] = 1 - self.m
+        return self.loss(a * (inp - sigma) * self.gamma, label)
+
+
+if __name__ == "__main__":
+    feature = torch.rand(32, 128)
+    gt = torch.randint(high=9, dtype=torch.long, size=(32,))
+    norm_liner = NormLinear(128, 10)
+    loss = CircleLoss(0.35, 256)
+    print(loss(norm_liner(feature), gt))
