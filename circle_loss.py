edf32c4223580ca849caa46d04f3eb9650870b9e
tzm <1049793129@qq.com>
2020-03-31 18:55:18 +0800

    fix circle_loss.py

--- circle_loss.py
+++ circle_loss.py
@@ -21,9 +21,14 @@ class CircleLoss(nn.Module):
 
     def forward(self, inp: Tensor, label: Tensor) -> Tensor:
         a = torch.clamp_min(inp + self.m, min=0).detach()
-        a[label] = torch.clamp_min(- inp[label] + 1 + self.m, min=0).detach()
+        src = torch.clamp_min(
+            - inp.gather(dim=1, index=label.unsqueeze(1)) + 1 + self.m,
+            min=0,
+        ).detach()
+        a.scatter_(1, label.unsqueeze(1), src)
         sigma = torch.ones_like(inp, device=inp.device, dtype=inp.dtype) * self.m
-        sigma[label] = 1 - self.m
+        src = torch.ones_like(label.unsqueeze(1), dtype=inp.dtype, device=inp.device) - self.m
+        sigma.scatter_(1, label.unsqueeze(1), src)
         return self.loss(a * (inp - sigma) * self.gamma, label)
 
 
@@ -31,5 +36,5 @@ if __name__ == "__main__":
     feature = torch.rand(32, 128)
     gt = torch.randint(high=10, dtype=torch.long, size=(32,))
     norm_liner = NormLinear(128, 10)
-    loss = CircleLoss(0.35, 256)
+    loss = CircleLoss(0.25, 256)
     print(loss(norm_liner(feature), gt))
