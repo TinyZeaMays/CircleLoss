fix overflow with autograd

circle_loss.py
@@ -4,37 +4,6 @@ import torch
 from torch import nn, Tensor
 
 
-class NormLinear(nn.Linear):
-    def __init__(self, in_features: int, out_features: int) -> None:
-        super(NormLinear, self).__init__(in_features, out_features, bias=False)
-
-    def forward(self, inp: Tensor) -> Tensor:
-        return nn.functional.linear(nn.functional.normalize(inp),
-                                    nn.functional.normalize(self.weight))
-
-
-class CircleLossLikeCE(nn.Module):
-    def __init__(self, m: float, gamma: float) -> None:
-        super(CircleLossLikeCE, self).__init__()
-        self.m = m
-        self.gamma = gamma
-        self.loss = nn.CrossEntropyLoss()
-
-    def forward(self, inp: Tensor, label: Tensor) -> Tensor:
-        a = torch.clamp_min(inp + self.m, min=0).detach()
-        src = torch.clamp_min(
-            - inp.gather(dim=1, index=label.unsqueeze(1)) + 1 + self.m,
-            min=0,
-        ).detach()
-        a.scatter_(dim=1, index=label.unsqueeze(1), src=src)
-
-        sigma = torch.ones_like(inp, device=inp.device, dtype=inp.dtype) * self.m
-        src = torch.ones_like(label.unsqueeze(1), dtype=inp.dtype, device=inp.device) - self.m
-        sigma.scatter_(dim=1, index=label.unsqueeze(1), src=src)
-
-        return self.loss(a * (inp - sigma) * self.gamma, label)
-
-
 def convert_label_to_similarity(normed_feature: Tensor, label: Tensor) -> Tuple[Tensor, Tensor]:
     similarity_matrix = normed_feature @ normed_feature.transpose(1, 0)
     label_matrix = label.unsqueeze(1) == label.unsqueeze(0)
@@ -48,11 +17,12 @@ def convert_label_to_similarity(normed_feature: Tensor, label: Tensor) -> Tuple[
     return similarity_matrix[positive_matrix], similarity_matrix[negative_matrix]
 
 
-class CircleLossBackward(nn.Module):
+class CircleLoss(nn.Module):
     def __init__(self, m: float, gamma: float) -> None:
-        super(CircleLossBackward, self).__init__()
+        super(CircleLoss, self).__init__()
         self.m = m
         self.gamma = gamma
+        self.soft_plus = nn.Softplus()
 
     def forward(self, sp: Tensor, sn: Tensor) -> Tensor:
         ap = torch.clamp_min(- sp.detach() + 1 + self.m, min=0.)
@@ -64,20 +34,9 @@ class CircleLossBackward(nn.Module):
         logit_p = - ap * (sp - delta_p) * self.gamma
         logit_n = an * (sn - delta_n) * self.gamma
 
-        loss = torch.log(1 + torch.clamp_max(torch.exp(logit_n).sum() * torch.exp(logit_p).sum(), max=1e38))
-        z = - torch.exp(- loss) + 1
+        loss = self.soft_plus(torch.logsumexp(logit_n, dim=0) + torch.logsumexp(logit_p, dim=0))
 
-        """
-        Eq. 10:
-        sp.backward(gradient=z * (- ap) * torch.softmax(- logit_p, dim=0) * self.gamma, retain_graph=True)
-        I modified it to 
-        sp.backward(gradient=z * (- ap) * torch.softmax(logit_p, dim=0) * self.gamma, retain_graph=True)
-        """
-
-        sp.backward(gradient=z * (- ap) * torch.softmax(logit_p, dim=0) * self.gamma, retain_graph=True)
-        sn.backward(gradient=z * an * torch.softmax(logit_n, dim=0) * self.gamma)
-
-        return loss.detach()
+        return loss
 
 
 if __name__ == "__main__":
@@ -86,7 +45,7 @@ if __name__ == "__main__":
 
     inp_sp, inp_sn = convert_label_to_similarity(feat, lbl)
 
-    circle_loss_backward = CircleLossBackward(m=0.25, gamma=256)
-    circle_loss = circle_loss_backward(inp_sp, inp_sn)
+    criterion = CircleLoss(m=0.25, gamma=256)
+    circle_loss = criterion(inp_sp, inp_sn)
 
     print(circle_loss)

mnist_example.py
@@ -8,7 +8,7 @@ from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 from tqdm import tqdm
 
-from circle_loss import CircleLossBackward, convert_label_to_similarity
+from circle_loss import convert_label_to_similarity, CircleLoss
 
 
 def get_loader(is_train: bool, batch_size: int) -> DataLoader:
@@ -44,7 +44,7 @@ def main(resume: bool = True) -> None:
     optimizer = SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-5)
     train_loader = get_loader(is_train=True, batch_size=64)
     val_loader = get_loader(is_train=False, batch_size=2)
-    loss_backward = CircleLossBackward(m=0.25, gamma=256)
+    criterion = CircleLoss(m=0.25, gamma=80)
 
     if resume and os.path.exists("resume.state"):
         model.load_state_dict(torch.load("resume.state"))
@@ -53,14 +53,15 @@ def main(resume: bool = True) -> None:
             for img, label in tqdm(train_loader):
                 model.zero_grad()
                 pred = model(img)
-                loss_backward(*convert_label_to_similarity(pred, label))
+                loss = criterion(*convert_label_to_similarity(pred, label))
+                loss.backward()
                 optimizer.step()
         torch.save(model.state_dict(), "resume.state")
 
     tp = 0
     fn = 0
     fp = 0
-    thresh = 0.8
+    thresh = 0.75
     for img, label in val_loader:
         pred = model(img)
         gt_label = label[0] == label[1]
@@ -77,4 +78,4 @@ def main(resume: bool = True) -> None:
 
 
 if __name__ == "__main__":
-    main(False)
+    main()